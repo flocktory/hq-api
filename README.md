87b0b869d55935d264c5cacb3660dddc278f7c24
Sergey Viskov <svoksiv@yandex.ru>
2022-04-06 13:50:37 +0300

    Add Example Spec and OpenApi Server Stub Generation

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# hq-api
+# HQ-API
 HQ OpenAPI Specification and Client &amp; Server SDK Generators
+
+### Cloning Github Repository
+* [Get access to Flocktory team in Github](https://github.com/flocktory)
+* [Adding a new SSH key to your GitHub account](https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account)
+* Clone Repository
+```
+$ git clone git@github.com:flocktory/hq-api.git
+$ cd hq-api
+```
+### Local Requirements 
+* Install [Java 13](https://stackoverflow.com/questions/52524112/how-do-i-install-java-on-mac-osx-allowing-version-switching#:~:text=Install%20manually%20from%20OpenJDK%20download,expected%20location%20of%20JDK%20installs.)
+#### Work Guarantied With:
+```
+$ java --version
+openjdk 13.0.1 2019-10-15
+OpenJDK Runtime Environment (build 13.0.1+9)
+OpenJDK 64-Bit Server VM (build 13.0.1+9, mixed mode, sharing)
+```
+### Server Stub for Spring JAR Generation 
+```
+# From repository root dir
+$ ./gradlew openApiGenerate
+$ mv build/generated/src/main/ ./src/main
+$ ./gradlew jar
+```
+### Documentation
+* [Swagger](https://swagger.io/)
+* [OpenAPI Specification](https://swagger.io/specification/)
+* [Gradle](https://docs.gradle.org/current/userguide/userguide.html)
+* [Openapi Gradle Generator ](https://openapi-generator.tech/docs/plugins/)
+* [Openapi Gradle Generator Spring](https://openapi-generator.tech/docs/generators/spring/)
+* [Spring](https://docs.spring.io/spring-boot/docs/current/reference/html/index.html)
+* [Spring boot](https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features)
+* [Buildkite](https://buildkite.com/docs)
+* [Sonarqube](https://docs.sonarqube.org/latest/)
---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    java
+    kotlin("jvm") version "1.5.31"
+    id("org.openapi.generator") version "5.4.0"
+    id("maven-publish")
+}
+
+val mavenName = System.getenv("MAVEN_NAME") ?: "api.hq"
+group = System.getenv("MAVEN_GROUP") ?: "ru.flocktory"
+version = System.getenv("MAVEN_VERSION") ?: "0.0.1"
+val mavenLink = System.getenv("MAVEN_LINK") ?: ""
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.springframework.boot:spring-boot-starter-web:2.6.2")
+    implementation("org.springframework.data:spring-data-commons:2.6.0")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
+    implementation("com.google.code.findbugs:jsr305:3.0.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.1")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.2")
+    implementation("org.springframework.boot:spring-boot-starter-validation:2.6.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
+}
+
+openApiGenerate {
+    generatorName.set("spring")
+    inputSpec.set("$rootDir/spec/api-hq.yaml")
+    outputDir.set("$buildDir/generated")
+    apiPackage.set("$group.$mavenName.api")
+    invokerPackage.set("$group.$mavenName.invoker")
+    modelPackage.set("$group.$mavenName.model")
+    configOptions.set(
+        mapOf(
+            "dateLibrary" to "java8",
+            "delegatePattern" to "true",
+        )
+    )
+}
+
+
+
+publishing {
+    repositories {
+        maven {
+            url = uri(mavenLink)
+            credentials(HttpHeaderCredentials::class) {
+                name = "Job-Token"
+                value = System.getenv("CI_JOB_TOKEN")
+            }
+            authentication {
+                create<HttpHeaderAuthentication>("header")
+            }
+        }
+    }
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+        }
+    }
+}
